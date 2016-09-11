2f6b4734f2740aaf49633748c81ef9db106760ca
MORI Shingo <babydaemons@gmail.com>
2016-09-11 01:50:23 +0000

    Solve exercise chapter 7

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,5 +7,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post users_path, user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert-danger'
+  end
+
+  test "valid signup infomation" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password" }
+    end
+    assert_template 'users/show'
+    assert flash.key?(:success)
   end
 end
