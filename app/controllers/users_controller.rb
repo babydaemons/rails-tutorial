fd343b065c5cd11f944bf93930cd218f62bbb52c
MORI Shingo <babydaemons@gmail.com>
2016-09-15 11:03:40 +0000

    Write update and edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_param)
+    @user = User.new(user_params)
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
@@ -18,8 +18,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功したときの処理
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
-    def user_param
+    def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
